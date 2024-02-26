723455ece12133fdf77c527a808509a884cc837e
Kunji Lal Meena <kunjilal976@gmail.com>
2024-02-26 23:14:35 +0530

    first commit

---
+++ src/components/Task.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './style.css';
+    
+function Task({ task, index, completeTask, removeTask }) {
+        return (
+            <div
+                className="task"
+                style={{ textDecoration: task.completed ? "line-through" : "" }}
+            >
+                {task.title}
+                <button style={{ background: "red" }} onClick={() => removeTask(index)}>x</button>
+                <button onClick={() => completeTask(index)}>Complete</button>
+            </div>
+        );
+    }
+    
+    // [...]
+
+
+export default Task
